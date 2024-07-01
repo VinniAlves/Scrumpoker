3665bd531b428ee1df5490980303021beda44d74
Vinicius de Lima Alves <vinicius1delimaalves@gmail.com>
2024-07-01 11:55:30 -0300

    Version 0.0.011

--- Front-end/src/assets/DadosMocados/esquemaDeVotacao.ts
+++ Front-end/src/assets/DadosMocados/esquemaDeVotacao.ts
@@ -1,4 +1,23 @@
 
 const esquemaVotacao:number[] = [1,2,3,5,8]
 
-export default esquemaVotacao
+const OpcoesVotos = [
+    {
+        'name': 'Fibonacci',
+        'size': [0,1,2,3,5,6,8,13,21,34,55,89,'?']
+    },
+    {
+        'name': 'Modiffied Fibanacci',
+        'size': [0,1,2,3,5,6,8,13,20,40,100,'?']
+    },
+    {
+        'name': 'T-shirts',
+        'size': ['XS','S','M','L','XL','?']
+    },
+    {
+        'name': 'Powers of 2',
+        'size': [0,1,2,3,4,8,16,32,64,'?']
+    },
+]
+
+export default OpcoesVotos
--- Front-end/src/screens/SalaPoker/PaginaConfiguracao/components/InputConfig/InputConfig.tsx
+++ Front-end/src/screens/SalaPoker/PaginaConfiguracao/components/InputConfig/InputConfig.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react'
 import styles from './InputConfig.module.css'
-
+import OpcoesVotos from '../../../../../assets/DadosMocados/esquemaDeVotacao'
 
 function InputConfig(){
+
+    const  [openOptions, setOpenOptions] = useState(true);
+    const [inputEscolha, setInputEscolha] = useState('');
+
+    function OpenModalVote(){
+        if(openOptions){
+            setOpenOptions(false)
+        }else{
+            setOpenOptions(true)
+        }
+
+    }
+
+
     return(
         <>
             <div className={styles.container}>
@@ -11,9 +26,32 @@ function InputConfig(){
                     <input></input>
                 </div>
                 
-                <input></input>
-                <input></input>
-                <button>Criar Jogo</button>
+                <div className={styles.inputSisVoto}>
+                    <p>Sistema de Voto</p>
+                    <input readOnly className={styles.SisButton} onClick={OpenModalVote} value={inputEscolha}></input>
+                </div>
+               {
+                openOptions ? 
+                
+                <div className={styles.optionMenu}>
+                    
+                { OpcoesVotos.map((index)=>(
+                        <button onClick={()=>{setInputEscolha(`${index.name} (${index.size})`)}}>{`${index.name} (${index.size})`}</button>
+                    ))
+                }
+                <button className={styles.btnEscolha}>Criar cartas customizadas...</button>
+                    
+
+                </div>
+                
+                : null
+               }
+                
+                <div className={styles.inputNomeSala}>
+                    <p>Quem pode revelar os votos</p>
+                    <input></input>
+                </div>
+                <button className={styles.ButtonContainer}>Criar Jogo</button>
             </div>
 
         </>
